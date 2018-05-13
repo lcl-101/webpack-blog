862d621eed4ad2f916077eed3dccb7dc59f73a68
lichenglong <554675188@qq.com>
2018-05-13 21:33:00 +0800

    首页移动站兼容处理

--- src/module/index/containers/App.js
+++ src/module/index/containers/App.js
@@ -11,7 +11,7 @@ class App extends Component{
         <div className={style.app_inner}>
           {this.props.children}
         </div>
-          <Footer/>
+        <Footer/>
       </div>
     )
   }
