93d4383cf2b294df361b6058579cf4188a862964
lichenglong <554675188@qq.com>
2018-05-24 14:21:59 +0800

    ++

--- src/module/index/component/Detail/Detail.js
+++ src/module/index/component/Detail/Detail.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Theader from '../Theader/Theader';
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import hljs from 'highlight.js';
 import marked from 'marked';
 import {fetchPostsIfNeeded} from '../../action/action'
 import style from '../../css/detail.less'
@@ -16,6 +17,13 @@ class Detail extends Component{
     const { dispatch} = this.props
     // 这里可以传两个值，一个是 reactjs 一个是 frontend
     dispatch(fetchPostsIfNeeded('frontend'));
+    window.scrollTo(0, 0);
+    // 代码高亮
+    marked.setOptions({
+      highlight: code => {
+        return hljs.highlightAuto(code).value;
+      }
+    });
   }
   render(){
     let view = [];
