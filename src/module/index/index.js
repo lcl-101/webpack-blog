2abb85da1c031b3fd6c7befdb35d6b0b64f3377d
lichenglong <554675188@qq.com>
2018-05-29 17:36:47 +0800

    ++

--- src/module/index/index.js
+++ src/module/index/index.js
@@ -13,25 +13,11 @@ const createStoreWithMiddleware = applyMiddleware(
   createLogger()
 )(createStore)
 
-function configureStore(initialState){
-  const store = createStoreWithMiddleware(todoApp, initialState)
-
-   if (module.hot) {
-     // Enable Webpack hot module replacement for reducers
-     module.hot.accept('../reducers', () => {
-       const nextRootReducer = require('../reducers')
-       store.replaceReducer(nextRootReducer)
-     })
-   }
-
-   return store
-}
-
 // 创建store, 传入两个参数
 // 参数1: reducer 用来修改state
 // 参数2(可选): [], 默认的state值,如果不传, 则为undefined
-let store = configureStore()
-
+// let store = configureStore()
+const store = createStoreWithMiddleware(todoApp)
 // 每次 state 更新时，打印日志
 // 注意 subscribe() 返回一个函数用来注销监听器
 // const unsubscribe = store.subscribe(() => {
