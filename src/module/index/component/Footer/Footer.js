c32d72ccb213a2168f1fc3b9bfc8ad8197e62bd2
lichenglong <554675188@qq.com>
2019-02-12 15:33:39 +0800

    ++

--- src/module/index/component/Footer/Footer.js
+++ src/module/index/component/Footer/Footer.js
@@ -9,7 +9,7 @@ class Footer extends Component {
           <a target="_blank" href="https://www.zhihu.com/people/li-cheng-long-3-99" className={style.footer_li}>知</a>
           <a target="_blank" href="http://weibo.com/3932790473" className={style.footer_li}><span className="icon iconfont icon-weibo"></span></a>
           <a target="_blank" href="https://www.facebook.com/100009375981606" className={style.footer_li}><span className="icon iconfont icon-facebook"></span></a>
-          <a target="_blank" href="https://www.facebook.com/100009375981606" className={style.footer_li}><span className="icon iconfont icon-github"></span></a>
+          <a target="_blank" href="https://github.com/lcl-101" className={style.footer_li}><span className="icon iconfont icon-github"></span></a>
         </div>
         <div className={style.footer_Copy}>Copyright © lichenglong Blog 2018</div>
       </div>
