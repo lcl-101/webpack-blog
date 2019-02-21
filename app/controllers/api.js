7a249564f62de237711dd46f4a55796c24f670ea
lichenglong <554675188@qq.com>
2019-02-21 16:09:52 +0800

    ++

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -82,16 +82,9 @@ module.exports.getReslog = async function (ctx, next) {
     message: null,
     errType: null
   };
-  function toJSONStr(str) {
-    return str.replace(/([\$\w]+)\s*:/g, function(_, $1){return '"'+$1+'":'});
-  }
-  function toJSON(str) {
-    return JSON.parse(str);
-  }
-
   try {
     var res = fs.readFileSync('/var/www/log4/blog/jsonlog/rule-res-json.log', 'utf8');
-    res = toJSON(toJSONStr(res));
+    res = res.substr(0, res.length - 1);
     resData.data= res;
   } catch(e) {
     resData.message = '数据请求失败';
