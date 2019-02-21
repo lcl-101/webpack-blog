128fbe1474ac95edeeb696b243700f340e23396f
lichenglong <554675188@qq.com>
2019-02-21 15:03:56 +0800

    ++

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -78,13 +78,13 @@ module.exports.getOauthAccesstoken = async function (ctx, next) {
 module.exports.getReslog = async function (ctx, next) {
   let resData = {
     status: 1,
-    data: '',
+    data: [],
     message: null,
     errType: null
   };
   try {
     var res = fs.readFileSync('/var/www/log4/blog/jsonlog/rule-res-json.log', 'utf8');
-    resData.data = res;
+    resData.data.push(res);
   } catch(e) {
     resData.message = '数据请求失败';
     resData.errType = 1001;
