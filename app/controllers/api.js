0ba7aca6e8cf2d59cdd1b83854cbe10dc7228dcc
lichenglong <554675188@qq.com>
2019-02-21 11:58:27 +0800

    ++

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -6,10 +6,6 @@ const client = redis.createClient('6379', '127.0.0.1');
 const service = 'https://api.github.com';
 const fs = require('fs');
 
-fs.readFile('/var/www/log4/blog/rule-res.log', 'utf8', function(err, data){
-  console.log(data);
-});
-
 //test redis
 client.set('hello', {a:1, b:2}) // 注意，value会被转为字符串,所以存的时候要先把value 转为json字符串
 client.get('hello', function(err, value){
@@ -77,11 +73,12 @@ module.exports.getOauthAccesstoken = async function (ctx, next) {
   }
   ctx.body = resData;
 }
+
 // getReslog
 module.exports.getReslog = async function (ctx, next) {
   let resData = '';
   try {
-    var res = await axios.get('/var/www/log4/blog/rule-res.log');
+    var res = fs.readFileSync('/var/www/log4/blog/rule-res.log', 'utf8');
     resData = res;
   } catch(e) {
     resData.message = '数据请求失败';
