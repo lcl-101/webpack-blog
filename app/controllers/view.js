6be684520df964e35d410de59f880b88aad7a163
lichenglong <554675188@qq.com>
2019-02-28 11:17:38 +0800

    ++

--- app/controllers/view.js
+++ app/controllers/view.js
@@ -1,24 +1,28 @@
 // Home
 module.exports.Home = async function (ctx, next) {
-  await ctx.render('index');
+  await ctx.render('views/index/index');
 }
 // Posts
 module.exports.Posts = async function (ctx, next) {
-  await ctx.render('index');
+  await ctx.render('views/index/index');
 }
 // Archive
 module.exports.Archive = async function (ctx, next) {
-  await ctx.render('index');
+  await ctx.render('views/index/index');
 }
 // Label
 module.exports.Label = async function (ctx, next) {
-  await ctx.render('index');
+  await ctx.render('views/index/index');
 }
 // Detail
 module.exports.Detail = async function (ctx, next) {
-  await ctx.render('index');
+  await ctx.render('views/index/index');
 }
 // Admin
 module.exports.Admin = async function (ctx, next) {
   await ctx.render('admin/index');
+}
+// Admin
+module.exports.Test = async function (ctx, next) {
+  await ctx.render('views/test/test');
 }
--- app/router/view.js
+++ app/router/view.js
@@ -6,9 +6,12 @@ viewRouter.get('/Home', viewHandlers.Home);
 viewRouter.get('/Posts', viewHandlers.Posts);
 viewRouter.get('/Archive', viewHandlers.Archive);
 viewRouter.get('/Label', viewHandlers.Label);
-viewRouter.get('/:id', viewHandlers.Detail);
-
+// test
+viewRouter.get('/test', viewHandlers.Test);
 // admin
 viewRouter.get('/admin', viewHandlers.Admin);
+// 详情
+viewRouter.get('/:id', viewHandlers.Detail);
+
 
 module.exports = viewRouter;
