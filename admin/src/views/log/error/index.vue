df77285155acb2a24663b3cb1fb5c989ddcfaa22
lichenglong <554675188@qq.com>
2019-03-13 17:30:42 +0800

    ++

--- admin/src/views/log/error/index.vue
+++ admin/src/views/log/error/index.vue
@@ -9,11 +9,15 @@
       <el-table-column prop="data[0].request-query" label="query"></el-table-column>
       <!-- <el-table-column prop="data[0].response-status" label="status" width="100"></el-table-column> -->
       <el-table-column prop="data[0].response-time" label="time(ms)" width="60"></el-table-column>
-      <el-table-column prop="data[0].user-agent" label="agent" width="400"></el-table-column>
+      <el-table-column prop="data[0].user-agent" label="agent" width="100"></el-table-column>
       <el-table-column prop="data[0].err-name" label="name" width="100"></el-table-column>
       <el-table-column prop="data[0].err-message" label="message" width="100"></el-table-column>
-      <el-table-column prop="data[0].err-stack" label="stack" width="100"></el-table-column>
-      <el-table-column prop="level.levelStr" label="level" width="90"></el-table-column>
+      <el-table-column prop="data[0].err-stack" label="stack" width="400"></el-table-column>
+      <el-table-column prop="level.levelStr" label="level" width="90">
+        <template slot-scope="scope">
+          <el-tag :type="scope.row.level.levelStr == 'INFO'?'success':'danger'">{{ scope.row.level.levelStr }}</el-tag>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
