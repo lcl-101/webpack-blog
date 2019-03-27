94f152ad947fac5a0dbc90249525319506723d51
lichenglong <554675188@qq.com>
2019-03-27 14:50:13 +0800

    ++

--- src/module/index/containers/rootPro/Router.js
+++ src/module/index/containers/rootPro/Router.js
@@ -3,35 +3,35 @@ import Loadable from 'react-loadable';
 import App from '../App';
 import Loading from '../../component/Loading/Loading'
 const Home = Loadable({
-  loader: () => import(/* webpackChunkName: "index/home" */'../../component/Home/Home.js'),
+  loader: () => import(/* webpackChunkName: "index/home" */'../../component/Home/Home.jsx'),
   loading: Loading,
 });
 const About = Loadable({
-  loader: () => import(/* webpackChunkName: "index/about" */'../../component/About/About.js'),
+  loader: () => import(/* webpackChunkName: "index/about" */'../../component/About/About.jsx'),
   loading: Loading,
 });
 const Posts = Loadable({
-  loader: () => import(/* webpackChunkName: "index/posts" */'../../component/Posts/Posts.js'),
+  loader: () => import(/* webpackChunkName: "index/posts" */'../../component/Posts/Posts.jsx'),
   loading: Loading,
 });
 const Archive = Loadable({
-  loader: () => import(/* webpackChunkName: "index/archive" */'../../component/Archive/Archive.js'),
+  loader: () => import(/* webpackChunkName: "index/archive" */'../../component/Archive/Archive.jsx'),
   loading: Loading,
 });
 const Label = Loadable({
-  loader: () => import(/* webpackChunkName: "index/label" */'../../component/Label/Label.js'),
+  loader: () => import(/* webpackChunkName: "index/label" */'../../component/Label/Label.jsx'),
   loading: Loading,
 });
 const Projects = Loadable({
-  loader: () => import(/* webpackChunkName: "index/projects" */'../../component/Projects/Projects'),
+  loader: () => import(/* webpackChunkName: "index/projects" */'../../component/Projects/Projects.jsx'),
   loading: Loading,
 });
 const Tags = Loadable({
-  loader: () => import(/* webpackChunkName: "index/tags" */'../../component/Tags/Tags'),
+  loader: () => import(/* webpackChunkName: "index/tags" */'../../component/Tags/Tags.jsx'),
   loading: Loading,
 });
 const Detail = Loadable({
-  loader: () => import(/* webpackChunkName: "index/detail" */'../../component/Detail/Detail'),
+  loader: () => import(/* webpackChunkName: "index/detail" */'../../component/Detail/Detail.jsx'),
   loading: Loading,
 });
 
